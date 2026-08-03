a5d0fcf9e1de3164b623f33b9f88b62ce4efadc4
Harsh Maisuriya <maisuriya.harsh2003@gmail.com>
2026-08-03 19:41:56 +0530

    small chnages in frontend

---
+++ frontend/src/routes/router.js
@@ -0,0 +1,89 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import RootLayout from "../layouts/RootLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import PublicRoute from "./PublicRoute";
+
+import Register from "../pages/Register";
+import VerifyEmail from "../pages/VerifyEmail";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+import UpdateProfile from "../pages/UpdateProfile";
+import ChangePassword from "../pages/ChangePassword";
+import ErrorPage from "../pages/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      // Default Route
+      {
+        index: true,
+        element: <Navigate to="/login" replace />,
+      },
+
+      // ==========================
+      // Public Routes
+      // ==========================
+      {
+        element: <PublicRoute />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "register",
+            element: <Register />,
+          },
+          {
+            path: "verify-email",
+            element: <VerifyEmail />,
+          },
+          {
+            path: "forgot-password",
+            element: <ForgotPassword />,
+          },
+          {
+            path: "reset-password",
+            element: <ResetPassword />,
+          },
+        ],
+      },
+
+      // ==========================
+      // Protected Routes
+      // ==========================
+      {
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "update-profile",
+            element: <UpdateProfile />,
+          },
+          {
+            path: "change-password",
+            element: <ChangePassword />,
+          },
+        ],
+      },
+
+      // ==========================
+      // 404 Route
+      // ==========================
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
